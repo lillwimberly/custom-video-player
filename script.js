826db7a80f8ccc328ece8d80a9ebb3613ec14877
Lillian Wimberly <lillianwimberly@gmail.com>
2023-10-20 18:30:59 -0700

    added play pause stop functionality

---
+++ script.js
@@ -0,0 +1,52 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+
+// Play & pause video
+// because this video var is connected to an html5 video tag we have an API we can use on it and its properties and methods
+const toggleVideoStatus = () => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+
+// Update the play/pause icon
+const updatePlayIcon = () => {
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
+};
+
+// Update progress & timestamp
+const updateProgress = () => {
+  return true;
+};
+
+// Set video time to progress
+const setVideoProgress = () => {
+  return true;
+};
+
+// Stop video
+const stopVideo = () => {
+  video.currentTime = 0;
+  video.pause();
+};
+
+// Event listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+
+stop.addEventListener('click', stopVideo);
+
+progress.addEventListener('change', setVideoProgress);
